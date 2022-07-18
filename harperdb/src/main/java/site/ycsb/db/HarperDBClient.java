e4988269746411fa2a4950baffbfc082c74a1af2
Daniel Seybold <daniel.seybold@uni-ulm.de>
2022-07-18 11:44:51 +0200

    added stubs for HarperDB Binding

---
+++ harperdb/src/main/java/site/ycsb/db/HarperDBClient.java
@@ -0,0 +1,45 @@
+package site.ycsb.db;
+
+import site.ycsb.ByteIterator;
+import site.ycsb.DB;
+import site.ycsb.DBException;
+import site.ycsb.Status;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+public class HarperDBClient extends DB {
+
+
+  @Override
+  public void init() throws DBException {
+
+  }
+
+  @Override
+  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    return null;
+  }
+
+  @Override
+  public Status scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+    return null;
+  }
+
+  @Override
+  public Status update(String table, String key, Map<String, ByteIterator> values) {
+    return null;
+  }
+
+  @Override
+  public Status insert(String table, String key, Map<String, ByteIterator> values) {
+    return null;
+  }
+
+  @Override
+  public Status delete(String table, String key) {
+    return null;
+  }
+}
