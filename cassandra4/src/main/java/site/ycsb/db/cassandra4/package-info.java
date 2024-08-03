9525824f3c504460c7b09afebc0f672fcaaf5555
Jörg Domaschka <joerg.domaschka@benchant.com>
2024-08-03 14:24:28 +0200

    changes to cassandra binding to support new workload
    remove the following configuration options:
    - cassandra.maxconnections
    - cassandra.coreconnections
    - cassandra.requestconsistencylevel
    
    add the following configuration options:
    + cassandra.maxrequestsperconnection
    + cassandra.maxconnectionspernode
    + cassandra.threadsperiogroup
    + cassandra.readconsistencylevel
    + cassandra.writeconsistencylevel
    + cassandra.tracing: tracing for Cassandra4 Binding and Aerospike workload
    
    some more updates
    o explicitly set consistency to each statement in Cassandra binding
    - fix inconsistent use of language features for Cassandra
    - add more config options for cassandra and fix query method
    - remove check for delete not found for Cassandra binding

--- cassandra4/src/main/java/site/ycsb/db/cassandra4/package-info.java
+++ cassandra4/src/main/java/site/ycsb/db/cassandra4/package-info.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014, Yahoo!, Inc. All rights reserved.
+ * Copyright (c) 2024 benchANT GmbH. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
